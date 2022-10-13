3ab09a133d70b163ef215d9844e392d50c64c7b2
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2022-10-13 16:08:00 +0100

    Filled in methods

--- expression.py
+++ expression.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from fractions import Fraction
+import numpy as np
+from typing import Dict, Any, Optional
+
 
 class Expression:
 
@@ -7,53 +11,126 @@ class Expression:
     def hash(self):
         pass
 
+    # Calculus
+
     def diff(self, term: Variable) -> Expression:
+        """Differentiate this expression with respect to a variable"""
+
+    # Evaluation
+
+    def __call__(self, variable_map: Dict[Variable, Any]):
         pass
 
-    def __call__(self, x):
-        pass
+    # Algabraic methods
+
+    @staticmethod
+    def _sanitise(value: Any, operation_name: Optional[str]=None) -> Expression:
+        """ Takes inputs that might be numbers, expressions, or something else,
+        and return an Expression"""
+
+        if isinstance(value, (int, float, Fraction, np.ndarray)):
+            return Constant(value)
+        elif isinstance(value, Expression):
+            return value
+        else:
+            if operation_name is not None:
+                raise TypeError(f"Invalid input to operation '{operation_name}': {repr(value)}")
+            else:
+                raise TypeError(f"Invalid input to Expression object: {repr(value)}")
 
     def __add__(self, other):
-        pass
+        other = Expression._sanitise(other, "+")
+        return Plus(self, other)
 
     def __radd__(self, other):
-        pass
+        other = Expression._sanitise(other, "+")
+        return Plus(other, self)
 
     def __sub__(self, other):
-        pass
+        other = Expression._sanitise(other, "-")
+        return Minus(self, other)
 
     def __rsub__(self, other):
-        pass
+        other = Expression._sanitise(other, "-")
+        return Minus(other, self)
+
+    def __neg__(self):
+        return Neg(self)
 
     def __mul__(self, other):
-        pass
+        other = Expression._sanitise(other, "*")
+        return Times(self, other)
 
     def __rmul__(self, other):
-        pass
+        other = Expression._sanitise(other, "*")
+        return Times(other, self)
 
-    def __divmod__(self, other):
-        pass
+    def __truediv__(self, other):
+        other = Expression._sanitise(other, "/")
+        return Divide(self, other)
 
     def __rdiv__(self, other):
-        pass
+        other = Expression._sanitise(other, "/")
+        return Divide(other, self)
+
+    def __mod__(self, other):
+        other = Expression._sanitise(other, "%")
+        return Modulo(self, other)
+
+    def __rmod__(self, other):
+        other = Expression._sanitise(other, "%")
+        return Modulo(other, self)
 
     def __pow__(self, other):
-        pass
+        other = Expression._sanitise(other, "**")
+        return Power(self, other)
 
     def __rpow__(self, other):
-        pass
+        other = Expression._sanitise(other, "**")
+        return Power(other, self)
 
     def __abs__(self, other):
-        pass
+        return Abs(self)
+
+
+class Unary(Expression):
+    """ Base class for unary expression components"""
+    def __init__(self, a: Any):
+        self.a = Expression._sanitise(a)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.a})"
+
+
+class Binary(Expression):
+    """ Base class for binary expression components"""
+    def __init__(self, a: Any, b: Any):
+        self.a = Expression._sanitise(a)
+        self.b = Expression._sanitise(b)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.a}, {self.b})"
+
+
+class Constant(Expression):
+    """ Represents a constant"""
+    def __init__(self, value: Any):
+        # This should NOT be a subtype of Unary, because it is the result of
+        # Expression._sanitise when given a number
+        self.value = value
+
+    def __repr__(self):
+        return repr(self.value)
+
 
 class Variable(Expression):
-    pass
+    def __init__(self):
+        pass
 
 
-class Plus(Expression):
+class Plus(Binary):
     def __init__(self, a: Expression, b: Expression):
-        self.a = a
-        self.b = b
+        super().__init__(a, b)
 
     def diff(self, term: Variable):
         return Plus(self.a.diff(term), self.b.diff(term))
@@ -61,10 +138,10 @@ class Plus(Expression):
     def __call__(self, x):
         return self.a(x) + self.b(x)
 
-class Minus(Expression):
+
+class Minus(Binary):
     def __init__(self, a: Expression, b: Expression):
-        self.a = a
-        self.b = b
+        super().__init__(a, b)
 
     def diff(self, term: Variable):
         return Minus(self.a.diff(term), self.b.diff(term))
@@ -72,17 +149,32 @@ class Minus(Expression):
     def __call__(self, x):
         return self.a(x) - self.b(x)
 
-class Neg(Expression):
-    pass
 
-class Times(Expression):
-    pass
+class Neg(Unary):
+    def __init__(self, a: Expression):
+        super().__init__(a)
 
-class Divide(Expression):
-    pass
 
-class Power(Expression):
-    pass
+class Times(Binary):
+    def __init__(self, a: Expression, b: Expression):
+        super().__init__(a, b)
 
-class Abs(Expression):
-    pass
+
+class Divide(Binary):
+    def __init__(self, a: Expression, b: Expression):
+        super().__init__(a, b)
+
+
+class Modulo(Binary):
+    def __init__(self, a: Expression, b: Expression):
+        super().__init__(a, b)
+
+
+class Power(Binary):
+    def __init__(self, a: Expression, b: Expression):
+        super().__init__(a, b)
+
+
+class Abs(Unary):
+    def __init__(self, a: Expression):
+        super().__init__(a)
